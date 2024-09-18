35a84eda82e47c9fed1298590c57ba4c60e4eb54
JM Chanes <qwertyuiop.xd1@gmail.com>
2024-09-18 15:55:07 -0600

    Initial commit  Changes to be committed: 	new file:   .gitignore 	se copia el gitignore para los elementos no vitales 	que agrega eclipse
    	new file:   src/org/student/Student.java
    	se crea la clase Student asi como el paquete
    	student donde se agrega

---
+++ src/org/student/Student.java
@@ -0,0 +1,25 @@
+package org.student;
+
+public class Student {
+    String firstName;
+    String lastName;
+    int registration;
+    int grade;
+    int year;
+    
+    public void printFullName(){
+        //TODO implement
+     }//printFullName
+
+     public void isApproved(){
+         //TODO implement: should return true if grade >= 60
+     }//isApproved
+
+     public int changeYearIfApproved(){
+         //TODO implement: the student should advance to the next year if he/she grade is >= 60
+         // Make year = year + 1, and print "Congragulations" if the student has been approved
+         return 0;
+     }//changerYearIfApproved
+     
+}//Class Student
+
